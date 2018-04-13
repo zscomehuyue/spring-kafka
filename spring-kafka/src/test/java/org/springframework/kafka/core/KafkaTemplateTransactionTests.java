1529a4263a09acbae7dbe449c76453800bbad943
Artem Bilan <abilan@pivotal.io>
2018-04-13 16:25:31 -0400

    Fix Checkstyle violation
    https://build.spring.io/browse/SK-SK20X-39

--- spring-kafka/src/test/java/org/springframework/kafka/core/KafkaTemplateTransactionTests.java
+++ spring-kafka/src/test/java/org/springframework/kafka/core/KafkaTemplateTransactionTests.java
@@ -1,6 +1,5 @@
 /*
  * Copyright 2017-2018 the original author or authors.
- * Copyright 2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -177,8 +176,8 @@ public class KafkaTemplateTransactionTests {
 		KafkaTemplate<String, String> template = new KafkaTemplate<>(pf);
 		template.setDefaultTopic(STRING_KEY_TOPIC);
 		assertThatThrownBy(() -> template.send("foo", "bar"))
-			.isInstanceOf(IllegalStateException.class)
-			.hasMessageContaining("No transaction is in process;");
+				.isInstanceOf(IllegalStateException.class)
+				.hasMessageContaining("No transaction is in process;");
 	}
 
 	@Configuration
