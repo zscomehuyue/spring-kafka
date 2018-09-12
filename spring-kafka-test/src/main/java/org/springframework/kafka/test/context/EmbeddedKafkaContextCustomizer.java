4037ca5ee0e5ef549fd989927ec4594e8888e6a7
OArtyomov <oleg_artyomov@ukr.net>
2018-09-12 13:56:08 -0400

    GH-804: EmbedKafkaCtxCustom add hashCode & equals
    Resolves https://github.com/spring-projects/spring-kafka/issues/804
    
    GH-804: `hashCode()` and `equals() for `EmbeddedKafkaContextCustomizer`
    
    * Fix check style.
    
    * Code review changes
    
    (cherry picked from commit 95d442abaa63ca952b24861dea522bb00349d2df)
    
    # Conflicts:
    #	spring-kafka-test/src/main/java/org/springframework/kafka/test/context/EmbeddedKafkaContextCustomizer.java

--- spring-kafka-test/src/main/java/org/springframework/kafka/test/context/EmbeddedKafkaContextCustomizer.java
+++ spring-kafka-test/src/main/java/org/springframework/kafka/test/context/EmbeddedKafkaContextCustomizer.java
@@ -40,6 +40,7 @@ import org.springframework.util.StringUtils;
  *
  * @author Artem Bilan
  * @author Elliot Metsger
+ * @author Oleg Artyomov
  *
  * @since 1.3
  */
@@ -99,6 +100,20 @@ class EmbeddedKafkaContextCustomizer implements ContextCustomizer {
 		beanFactory.initializeBean(kafkaEmbedded, KafkaEmbedded.BEAN_NAME);
 		beanFactory.registerSingleton(KafkaEmbedded.BEAN_NAME, kafkaEmbedded);
 		((DefaultSingletonBeanRegistry) beanFactory).registerDisposableBean(KafkaEmbedded.BEAN_NAME, kafkaEmbedded);
+	}
+
+	@Override
+	public int hashCode() {
+		return this.embeddedKafka.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null || obj.getClass() != getClass()) {
+			return false;
+		}
+		EmbeddedKafkaContextCustomizer customizer = (EmbeddedKafkaContextCustomizer) obj;
+		return this.embeddedKafka.equals(customizer.embeddedKafka);
 	}
 
 }
